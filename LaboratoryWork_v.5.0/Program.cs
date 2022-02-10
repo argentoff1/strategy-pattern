b86c1881f794bd5e69ffcd7fec2ae5d7dac53021
istrebitelantox <geekpook@gmail.com>
2022-02-10 11:16:00 +0300

    Добавлено изменение поведения

--- LaboratoryWork_v.5.0/Program.cs
+++ LaboratoryWork_v.5.0/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MyLibrary;
 using MyLibrary.Interfaces;
 using MyLibrary.Classes;
+using MyLibrary.Classes.Behaviors;
 
 namespace LaboratoryWork_v._5._0
 {
@@ -17,6 +18,25 @@ namespace LaboratoryWork_v._5._0
             MallardDuck mallardDuck = new MallardDuck();
             RubberDuck rubberDuck = new RubberDuck();
             DecoyDuck decoyDuck = new DecoyDuck();
+
+            IQuackBehavior dQuack = new DQuack();
+            IQuackBehavior muteQuack = new MuteQuack();
+            IQuackBehavior squeak = new Squeak();
+
+            IFlyBehavior flyNoWay = new FlyNoWay();
+            IFlyBehavior flyWithWings = new FlyWithWings();
+
+            redheadDuck.SetFlyBehavior(flyWithWings);
+            redheadDuck.SetQuackBehavior(dQuack);
+
+            mallardDuck.SetFlyBehavior(flyWithWings);
+            mallardDuck.SetQuackBehavior(dQuack);
+
+            rubberDuck.SetFlyBehavior(flyNoWay);
+            rubberDuck.SetQuackBehavior(squeak);
+
+            decoyDuck.SetFlyBehavior(flyNoWay);
+            decoyDuck.SetQuackBehavior(muteQuack);
 
             Duck[] ducks = { redheadDuck, mallardDuck, decoyDuck, rubberDuck };
 
